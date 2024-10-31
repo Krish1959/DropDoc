dce5e82832b311905c4e801bb013ebb89509c9a5
UNNI <49119124+Krish1959@users.noreply.github.com>
2024-10-31 13:35:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,8 @@ st.write("Upload a document below and ask a question about it– GPT will answer
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 #openai_api_key = st.text_input("OpenAI API Key", type="password")
-openai_api_key = st.secrets["openai"]["API_KEY"]
+openai_api_key = st.secrets["openai"]["secret_key"]
+
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
